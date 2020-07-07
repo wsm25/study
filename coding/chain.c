d41c59bf14a1e9ee12e849eed43510f61654e9df
wsm <wsm@wsm.wsm>
2020-07-07 13:06:54 +0800

    upload ffile

--- coding/chain.c
+++ coding/chain.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
-#define N 200
-#define LOOP 3
+#define N 20000
+#define LOOP 32
 
 struct chain{
 	int num;
@@ -23,13 +23,14 @@ struct chain* initch(){
 
 int main(){
 	struct chain* ph=initch();
-	int i=0;
+	int i=-1; 
 	while(ph!=(ph->p)){
 		if(++i>=LOOP){
-			i=0;
+			i=1;
+			//printf("%d\t",ph->num);
 			ph->p=(ph->p)->p;
-			ph=ph->p;
 		}
+		ph=ph->p;
 	}
 	printf("%d\n",ph->num);
 	return 0;
