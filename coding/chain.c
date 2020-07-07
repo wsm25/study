6725fc607b6be1b6d4287b41664c6c1179b3ba30
wsm <wsm@wsm.wsm>
2020-07-07 11:31:14 +0800

    add file

--- coding/chain.c
+++ coding/chain.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <malloc.h>
-#define N 30
+#define N 200
 #define LOOP 3
 
 struct chain{
 	int num;
-	chain* p;
-}
+	struct chain* p;
+};
 
-chain* initch(){
-	chain* ph=(chain *)malloc(sizeof(chain));
-	chain* pb=ph;
+struct chain* initch(){
+	struct chain* ph=(struct chain *)malloc(sizeof(struct chain));
+	struct chain* pb=ph;
 	int i=1;
 	do{
 		pb->num=i;
-		pb->p=(chain *)malloc(sizeof(chain));
+		pb->p=(struct chain *)malloc(sizeof(struct chain));
 		pb=pb->p;
 	}while((++i)<=N-1);
 	pb->p=ph;
@@ -22,13 +22,13 @@ chain* initch(){
 }
 
 int main(){
-	chain* ph=initch();
-	chain* pi=NULL;
+	struct chain* ph=initch();
 	int i=0;
 	while(ph!=(ph->p)){
 		if(++i>=LOOP){
 			i=0;
 			ph->p=(ph->p)->p;
+			ph=ph->p;
 		}
 	}
 	printf("%d\n",ph->num);
